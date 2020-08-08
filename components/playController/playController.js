bbcbe6dc5d75628366b07e9f2f020c939faabc6b
codelei <972761675@qq.com>
2020-08-08 16:11:48 +0800

    详情页歌词同步显示

---
+++ components/playController/playController.js
@@ -0,0 +1,27 @@
+// components/playController/playController.js
+
+Component({
+  // attached() {
+  //   this.setData({songdetail: wx.getStorageSync('song_detail_info')});
+  // },
+  // pageLifetimes: {
+  //   show() {
+  //     if(wx.getStorageSync('song_detail_info').id != this.data.songdetail.id) {
+  //       this.setData({songdetail: wx.getStorageSync('song_detail_info')});
+  //     }
+  //   },
+  // },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    songdetail:{}
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -6,7 +6,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    songdetail:[],
+    songdetail:{},
+    songText:[],
+    songDateTime:[],
     state: true,
     music_length: "",
     current: "",
@@ -14,19 +16,19 @@ Page({
     State_number: 1,
     progress:0,
     songs:[],
-    currentId: 0
+    currentId: 0,
+    isLike: false,
+    Text:''
   },
   
   onLoad: function (options) {
     this.setData({
       songs: wx.getStorageSync('songs')
     })
-    // /* 初始化播放状态 */
-    // this.currentPauseState();
     /* 初始化播放音乐 */
     this.initMusic(options.ids);
     /* 监听是否是播放状态 */
-    innerAudioContext.paused ? this.resetInstance() : "";
+    innerAudioContext.paused ? this.resetInstance() : innerAudioContext.play();
     /* 监听音频是否播放结束 */
   },
   initMusic(id) {
@@ -37,6 +39,8 @@ Page({
       this.setData({
         songdetail: res.data.songs[0]
       })
+      // wx.setStorageSync('song_detail_info', this.data.songdetail)
+      /* 歌曲地址获取 */
       request({
         url:"/song/url?id="+this.data.songdetail.id
       }).then(res => {
@@ -61,20 +65,25 @@ Page({
               current: `0${minutes}:${this.format(seconds)}`,
               progress: progress
             });
+            const songTextIndex = this.data.songDateTime.indexOf(this.data.current);
+            if(songTextIndex > -1) {
+                this.setData({Text: this.data.songText[songTextIndex]});
+            }
+            
           });
           /* 播放结束进行下一首 */
           innerAudioContext.onEnded(() => {
             switch (this.data.State_number) {
               case 1:
-                console.log('顺序播放');
-                this.cardLoop(id);
+                console.log('顺序播放')
+                this.cardLoop();
                 break;
               case 2:
-                console.log('随机播放');
+                console.log('随机播放')
                 this.randomPause();
                 break;
               case 3:
-                console.log('单曲循环');
+                console.log('单曲循环')
                 innerAudioContext.play();
                 break;
               case 4: 
@@ -82,7 +91,25 @@ Page({
                 break;
             }
           });
-      })
+      });
+      /* 歌曲歌词获取 */
+      request({
+        url:'/lyric?id='+this.data.songdetail.id
+      }).then(res => {
+        if(res.data.lrc) {
+          const song_txt = res.data.lrc.lyric.split(/\[.*?\]/).filter(v => !!v);
+          const song_time = res.data.lrc.lyric.match(/[0-5][0-9]:[0-5][0-9]/g);
+          this.setData({
+            songText: song_txt,
+            songDateTime: song_time
+          });
+        } else {
+          this.setData({
+            songText: [],
+            songDateTime: []
+          });
+        }
+      });
     })
   },
   /* 播放模式状态开关 */
@@ -111,9 +138,8 @@ Page({
     this.currentPauseState();
   },
   /* 列表循环播放模式 */
-  cardLoop(id) {
-    const index = this.toggleHandel(id);
-    index+1 > this.data.songs.length ? this.initMusic(this.data.songs[0].id) : this.initMusic(this.data.songs[index+1].id);
+  cardLoop() {
+    this.nextMusic();
   },
   /* 随机播放模式 */
   randomPause() {
@@ -122,12 +148,14 @@ Page({
   },
   /* 上一首 */
   preMusic() {
+    this.setData({Text: ''}); // 防止歌词缓存
     this.data.state ? '' :  this.setData({state: true});
     const index = this.toggleHandel(this.data.currentId);
     index-1 < 0 ? this.initMusic(this.data.songs[this.data.songs.length-1].id) : this.initMusic(this.data.songs[index-1].id);
   },
   /* 下一首 */
   nextMusic() {
+    this.setData({Text: ''}); // 防止歌词缓存
     this.data.state ? '' :  this.setData({state: true});
     const index = this.toggleHandel(this.data.currentId);
     index+1 > this.data.songs.length - 1 ? this.initMusic(this.data.songs[0].id) : this.initMusic(this.data.songs[index+1].id);
@@ -136,7 +164,8 @@ Page({
     let index = 0;
     this.data.songs.forEach((song, i) => {
       if(song.id == id) {
-        return index = i;
+        index = i;
+        return;
       }
     });
     return index;
@@ -151,6 +180,12 @@ Page({
     this.setData({
       state: !this.data.state
     })
+  },
+  /* 歌曲添加进我喜欢的音乐 */
+  songHandelLike() {
+    this.setData({
+      isLike: !this.data.isLike
+    });
   },
   /* 格式校正 */
   format(val) {
